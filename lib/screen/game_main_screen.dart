c9b3b18d596450404184075d420d05aa4497c06e
jayurathod <jayr770101@gmail.com>
2022-07-01 16:51:36 +0530

    add game functionality

--- lib/screen/game_main_screen.dart
+++ lib/screen/game_main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum Winner { NONE, PLAYER1, PLAYER2 }
+
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
 
@@ -8,6 +10,8 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  Winner currentWinner = Winner.NONE;
+
   List gameIcon = [
     Icons.circle_rounded,
     Icons.circle_rounded,
@@ -19,8 +23,18 @@ class _GameScreenState extends State<GameScreen> {
     Icons.circle_rounded,
     Icons.circle_rounded,
   ];
+  List reStart = [
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+    Icons.circle_rounded,
+  ];
   bool isPlayer1 = false;
-  bool isClick = false;
 
   @override
   void initState() {
@@ -31,63 +45,208 @@ class _GameScreenState extends State<GameScreen> {
   void clickBox(index) {
     if (isPlayer1) {
       gameIcon[index] = Icons.close;
-    }
-    if (!isPlayer1) {
+    } else {
       gameIcon[index] = Icons.circle_outlined;
     }
-
     isPlayer1 = !isPlayer1;
+    winnerWinner();
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.purple, borderRadius: BorderRadius.circular(12)),
-            child: Card(
-                color: Colors.deepPurple.shade400,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                    ),
-                    itemCount: gameIcon.length,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          clickBox(index);
-                        },
-                        child: Container(
-                          margin: EdgeInsets.all(2),
-                          height: 60,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(10),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Chokdi Mindu"),
+          elevation: 0.0,
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 300,
+                  width: 300,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(12)),
+                  child: Card(
+                      color: Colors.deepPurple.shade400,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: GridView.builder(
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 4.0,
+                            crossAxisSpacing: 4.0,
                           ),
-                          child: Icon(
-                            gameIcon[index],
-                            color: Colors.white,
-                            size: gameIcon[index] == Icons.circle_rounded
-                                ? 10
-                                : 60,
-                          ),
-                          // child:Icons.circle_outlined,size: 70,),
+                          itemCount: gameIcon.length,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              onTap: () {
+                                clickBox(index);
+                              },
+                              child: Container(
+                                margin: EdgeInsets.all(2),
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Icon(
+                                  gameIcon[index],
+                                  color: Colors.white,
+                                  size: gameIcon[index] == Icons.circle_rounded
+                                      ? 10
+                                      : 60,
+                                ),
+                                // child:Icons.circle_outlined,size: 70,),
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
+                      )),
+                ),
+                SizedBox(height: 30),
+                InkWell(
+                  borderRadius: BorderRadius.circular(50),
+                  onTap: restartGame,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    child: Center(
+                      child: Icon(
+                        Icons.replay_outlined,
+                        size: 40.0,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
                   ),
-                )),
+                ),
+              ],
+            ),
           ),
         ));
+  }
+
+  void winnerWinner() {
+    // player 1 condition
+    if (gameIcon[0] == Icons.close &&
+        gameIcon[1] == Icons.close &&
+        gameIcon[2] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[3] == Icons.close &&
+        gameIcon[4] == Icons.close &&
+        gameIcon[5] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[6] == Icons.close &&
+        gameIcon[7] == Icons.close &&
+        gameIcon[8] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[0] == Icons.close &&
+        gameIcon[3] == Icons.close &&
+        gameIcon[6] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[1] == Icons.close &&
+        gameIcon[4] == Icons.close &&
+        gameIcon[7] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[2] == Icons.close &&
+        gameIcon[5] == Icons.close &&
+        gameIcon[8] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[0] == Icons.close &&
+        gameIcon[4] == Icons.close &&
+        gameIcon[8] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    } else if (gameIcon[2] == Icons.close &&
+        gameIcon[4] == Icons.close &&
+        gameIcon[6] == Icons.close) {
+      currentWinner = Winner.PLAYER1;
+    }
+
+    // player 2 condition
+    if (gameIcon[0] == Icons.circle_outlined &&
+        gameIcon[1] == Icons.circle_outlined &&
+        gameIcon[2] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[3] == Icons.circle_outlined &&
+        gameIcon[4] == Icons.circle_outlined &&
+        gameIcon[5] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[6] == Icons.circle_outlined &&
+        gameIcon[7] == Icons.circle_outlined &&
+        gameIcon[8] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[0] == Icons.circle_outlined &&
+        gameIcon[3] == Icons.circle_outlined &&
+        gameIcon[6] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[1] == Icons.circle_outlined &&
+        gameIcon[4] == Icons.circle_outlined &&
+        gameIcon[7] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[2] == Icons.circle_outlined &&
+        gameIcon[5] == Icons.circle_outlined &&
+        gameIcon[8] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[0] == Icons.circle_outlined &&
+        gameIcon[4] == Icons.circle_outlined &&
+        gameIcon[8] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    } else if (gameIcon[2] == Icons.circle_outlined &&
+        gameIcon[4] == Icons.circle_outlined &&
+        gameIcon[6] == Icons.circle_outlined) {
+      currentWinner = Winner.PLAYER2;
+    }
+
+    //shoe winning dialog box
+    if (currentWinner == Winner.PLAYER1) {
+      showDialogMessage("Player 1 is winner");
+      return;
+    }
+    if (currentWinner == Winner.PLAYER2) {
+      showDialogMessage("Player 2 is winner");
+      return;
+    }
+
+    bool isDraw = true;
+    gameIcon.forEach((element) {
+      if (element == Icons.circle_rounded) {
+        isDraw = false;
+      }
+    });
+
+    //shoe draw dialog box
+    if (isDraw) {
+      showDialogMessage("Game is draw");
+    }
+  }
+
+  //winning and drawing dialog box
+  showDialogMessage(text) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(text),
+        );
+      },
+    );
+  }
+
+  // restart game
+  void restartGame() {
+    currentWinner = Winner.NONE;
+    isPlayer1 = true;
+    gameIcon = [];
+    gameIcon.addAll(reStart);
+    setState(() {});
   }
 }
